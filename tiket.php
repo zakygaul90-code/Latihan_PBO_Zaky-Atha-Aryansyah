d54c0c186d09529f21ecfcdcb3e1a85daf6e3c7b
Zaky Atha Aryansyah <zaky.gaul90@gmail.com>
2026-06-15 14:10:42 +0700

    [Tahap 3] Membuat abstract class Tiket

---
+++ tiket.php
@@ -0,0 +1,23 @@
+<?php
+abstract class Tiket {
+    // Properti Terenkapsulasi menggunakan hak akses 'protected' sesuai instruksi
+    protected $id_tiket;
+    protected $nama_film;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $hargaDasarTiket; // Dipetakan dari kolom harga_dasar_tiket di database
+
+    // Constructor untuk memetakan nilai dari kolom database
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket) {
+        $this->id_tiket        = $id_tiket;
+        $this->nama_film       = $nama_film;
+        $this->jadwal_tayang   = $jadwal_tayang;
+        $this->jumlah_kursi    = $jumlah_kursi;
+        $this->hargaDasarTiket = $hargaDasarTiket;
+    }
+
+    // Metode Abstrak (Tanpa Isi/Body) yang wajib diturunkan dan diisi oleh kelas anak
+    abstract public function hitungTotalHarga();
+    abstract public function tampilkanInfoFasilitas();
+}
+?>
